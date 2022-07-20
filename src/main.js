833d4ae33c1abd99304d7f10deae961ce9975b95
Myhaushine <myhautran181299@gmail.com>
2022-07-21 00:17:24 +0700

    delete code in main.js

--- src/main.js
+++
@@ -1,7 +0,0 @@
-function showName() {
-  alert('hahaha');
-  // var results = document.getElementsByClassName('content_form_name_txt').value;
-  // document.getElementsByClassName('content_table').innerHTML = `<p>List name is: ${results}</p>`
-}
-showName();
-
